15956dcc32e8811251f5a92c274d7cd690bbc9d7
kuteera <mukeshsshetty44@gmail.com>
2022-08-20 19:43:54 +0530

    Test

--- src/newtesting/DemoProject.java
+++ src/newtesting/DemoProject.java
@@ -7,7 +7,7 @@ public class DemoProject {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Mukesh Test Project");
+		System.out.println("Mukesh Test Project ");
 
 	}
 
