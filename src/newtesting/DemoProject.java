0592aaaa1e977b359ea215d39b68af43d33e3f89
kuteera <mukeshsshetty44@gmail.com>
2022-08-20 19:24:33 +0530

    Mukesh Test Commit

---
+++ src/newtesting/DemoProject.java
@@ -0,0 +1,14 @@
+package newtesting;
+
+public class DemoProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mukesh Test Project");
+
+	}
+
+}
